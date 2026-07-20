f095549415418484bc46ee9369b3b2644b3ea567
RCBDAuto <kyllonmeves52@gmail.com>
2026-07-20 20:36:46 +0200

    Permissoes USSD

--- app/src/main/java/com/rcbd/auto/LicenseManager.java
+++ app/src/main/java/com/rcbd/auto/LicenseManager.java
@@ -89,108 +89,145 @@ public class LicenseManager {
 
 
 
-
-
     public static boolean ativar(
-            Context context,
-            String chave
-    ){
+        Context context,
+        String chave
+){
 
+    try{
 
-        try{
+        // VERIFICA SE JÁ EXISTE UMA LICENÇA
 
+        if (LicenseStorage.existe()) {
 
-            String[] partes =
-                    chave.split("-");
+            try {
 
+                String dados = LicenseStorage.ler();
 
+                if(dados != null){
 
-            if(partes.length != 4){
-                return false;
+                    String chaveSalva =
+                            pegar(dados, "CHAVE");
+
+                    if(chave.equalsIgnoreCase(chaveSalva)){
+                        return false;
+                    }
+
+                }
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
             }
-
-
-
-            if(!partes[0].equals("RCBD")){
-                return false;
-            }
-
-
-
-            String id =
-                    partes[1];
-
-            String dias =
-                    partes[2].replace("D","");
-
-            String codigo =
-                    partes[3];
-
-
-
-            if(!id.equals(ID_LICENCA)){
-                return false;
-            }
-
-
-            if(!dias.equals(
-                    String.valueOf(DIAS_PLANO)
-            )){
-                return false;
-            }
-
-
-            if(!codigo.equals(CODIGO)){
-                return false;
-            }
-
-
-
-
-            String idAndroid =
-                    Settings.Secure.getString(
-                            context.getContentResolver(),
-                            Settings.Secure.ANDROID_ID
-                    );
-
-
-
-            long inicio =
-                    System.currentTimeMillis();
-
-
-
-            long finalizacao =
-        inicio +
-        (10 * 60 * 1000L);
-
-
-
-            String dados =
-
-                    "CHAVE="+chave+"\n"+
-                    "ID_ANDROID="+idAndroid+"\n"+
-                    "DATA_INICIO="+inicio+"\n"+
-                    "DATA_FINAL="+finalizacao;
-
-
-
-            LicenseStorage.salvar(dados);
-
-
-
-            return true;
-
-
-
-        }catch(Exception e){
-
-            return false;
 
         }
 
 
+        String[] partes =
+                chave.split("-");
+
+
+        if(partes.length != 4){
+            return false;
+        }
+
+
+        if(!partes[0].equals("RCBD")){
+            return false;
+        }
+
+
+        String id =
+                partes[1];
+
+
+        String dias =
+                partes[2].replace("D","");
+
+
+        String codigo =
+                partes[3];
+
+
+
+        String idAndroid =
+                Settings.Secure.getString(
+                        context.getContentResolver(),
+                        Settings.Secure.ANDROID_ID
+                );
+
+
+        String idGerado =
+                gerarIdLicenca(idAndroid);
+
+
+
+        // Verifica ID da chave com ID do APK
+
+        if(!id.equals(ID_LICENCA)){
+            return false;
+        }
+
+
+
+        // Verifica ID do aparelho
+
+        if(!idGerado.equals(ID_LICENCA)){
+            return false;
+        }
+
+
+
+        if(!dias.equals(
+                String.valueOf(DIAS_PLANO)
+        )){
+            return false;
+        }
+
+
+
+        if(!codigo.equals(CODIGO)){
+            return false;
+        }
+
+
+
+        long inicio =
+                System.currentTimeMillis();
+
+
+
+        long finalizacao =
+                inicio +
+                (10 * 60 * 1000L);
+
+
+
+        String dados =
+
+                "CHAVE="+chave+"\n"+
+                "ID_ANDROID="+idAndroid+"\n"+
+                "DATA_INICIO="+inicio+"\n"+
+                "DATA_FINAL="+finalizacao;
+
+
+
+        LicenseStorage.salvar(dados);
+
+
+
+        return true;
+
+
+
+    }catch(Exception e){
+
+        return false;
+
     }
+
+}
 
 
 
@@ -280,6 +317,17 @@ public class LicenseManager {
         return "";
 
     }
+
+private static String gerarIdLicenca(String androidId) {
+
+    if(androidId == null || androidId.length() < 8){
+        return "";
+    }
+
+    return androidId
+            .substring(0, 8)
+            .toUpperCase(Locale.ROOT);
+}
 
 
 }

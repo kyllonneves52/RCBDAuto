fedb28c92182dc106716ca6b4aec9c36cd025003
RCBDAuto <kyllonmeves52@gmail.com>
2026-07-26 13:46:47 +0200

    Permissoes USSD

--- app/src/main/java/com/rcbd/auto/LicenseManager.java
+++ app/src/main/java/com/rcbd/auto/LicenseManager.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 public class LicenseManager {
 
     // ID FIXO E BUILD FIXO
-    private static final String BUILD_PERMITIDO = "RP1A.201005.001";
+    private static final String BUILD_PERMITIDO = "SP1A.210812.016";
     private static final String ID_LICENCA = "DF7A2792";
     private static final int DIAS_PLANO = 3;
     private static final String CODIGO = "AB95";
